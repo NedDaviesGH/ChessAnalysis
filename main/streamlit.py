68c984434d2cd70f7ee5a0d0ee610cabb2384187
neddaviesGROMACSking <ned.davies16@gmail.com>
2022-07-13 10:42:15 +0100

    First Commit

---
+++ main/streamlit.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+
+st.write("hello World")
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+st.line_chart(df)
